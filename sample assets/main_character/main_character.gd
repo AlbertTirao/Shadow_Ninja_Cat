14eeaee5dbcd0f4162aec372668bb71f7923e5f1
AlbertTirao <alli.tirao.up@phinmaed.com>
2025-02-06 16:19:37 +0800

    update UI
    nakakalito pero explore nalang

--- sample assets/main_character/main_character.gd
+++ sample assets/main_character/main_character.gd
@@ -1,12 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
+const SPRINT_SPEED = 500.0
+const DASH_SPEED = 800.0
 const JUMP_VELOCITY = -600.0
+const DASH_DURATION = 0.2
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
+
+var can_double_jump = false
+var is_dashing = false
+var dash_timer = 0.0
 
 
 func _physics_process(delta: float) -> void:
+	if is_dashing:
+		dash_timer -= delta
+		if dash_timer <= 0:
+			is_dashing =false
+		move_and_slide()
+		return
+		
 	if (velocity.x > 1 || velocity.x < -1):
 		sprite_2d.animation = "running"
 	else :
@@ -16,10 +30,20 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		sprite_2d.animation = "jumping"
+	else:
+		can_double_jump = true
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif can_double_jump:
+			velocity.y = JUMP_VELOCITY
+			can_double_jump = false
+			
+	var speed = SPEED
+	if Input.is_action_just_pressed("sprint"):
+		speed = SPRINT_SPEED
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -28,6 +52,11 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 12)
+		
+	if Input.is_action_just_pressed("dash") and not is_dashing:
+		is_dashing = true
+		dash_timer = DASH_DURATION
+		velocity.x = (1 if velocity.x > 0 else -1) * DASH_SPEED
 
 	move_and_slide()
 	
