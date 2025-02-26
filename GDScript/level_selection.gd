304519cba0f78a29291836f291110a1bdbcd5293
AlbertTirao <alli.tirao.up@phinmaed.com>
2025-02-26 23:01:41 +0800

    update UI

--- GDScript/level_selection.gd
+++ GDScript/level_selection.gd
@@ -17,3 +17,11 @@ func _on_back_pressed() -> void:
 
 func _on_level_1_pressed() -> void:
 	get_tree().change_scene_to_file("res://ACT-1_Levels/Level-1.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://ACT-1_Levels/Level-2.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://ACT-1_Levels/Level-3.tscn")
